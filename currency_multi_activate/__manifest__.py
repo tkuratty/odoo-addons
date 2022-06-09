31a025c39ef7f892ae5cef283c384adbf46a5514
tkuratty <47928719+tkuratty@users.noreply.github.com>
2022-06-09 11:26:20 +0000

    Generate addon base info.

---
+++ currency_multi_activate/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 TKuratty
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Currency Multi Activate',
+    'description': """
+        Activate currency by selected in tree view
+        選択した通貨を有効化する""",
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'TKuratty',
+    'website': 'https://github.com/tkuratty/odoo-addons/tree/main',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
