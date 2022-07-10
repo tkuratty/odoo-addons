5d95e0abdeea824ee6d8cfc23b2c40f274480550
tkuratty <47928719+tkuratty@users.noreply.github.com>
2022-07-10 21:15:27 +0000

    Check debug oepration

--- my_test_001/models/purchase_order.py
+++ my_test_001/models/purchase_order.py
@@ -7,3 +7,10 @@ from odoo import _, api, fields, models
 class PurchaseOrder(models.Model):
 
     _inherit = "purchase.order"
+    x_purchase_line_count = fields.Char(
+        string='Test Text', compute="_compute_line_count", store=True)
+
+    @api.depends('order_line')
+    def _compute_line_count(self):
+        for order in self:
+            order.x_purchase_line_count = len(order.order_line)
