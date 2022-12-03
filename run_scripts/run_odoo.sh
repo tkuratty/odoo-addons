6c33cd21cbb11c1046b6f91fe675ef794f48d7ef
Ratty123 <tkuratty@gmail.com>
2022-12-03 06:38:19 +0000

    Change db name

--- run_scripts/run_odoo.sh
+++ run_scripts/run_odoo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m debugpy --listen 0.0.0.0:8888 /usr/bin/odoo --db_host=db --db_port=5432 --db_user=odoo --db_password=odoo --database=Odoo14 --limit-time-real=100000 -u my_test_002
+python3 -m debugpy --listen 0.0.0.0:8888 /usr/bin/odoo --db_host=db --db_port=5432 --db_user=odoo --db_password=odoo --database=Odoo --limit-time-real=100000 -u my_test_002
